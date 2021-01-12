3903b450348d7aa4e2be6901b87fc0c589b43c65
Sophie Gu <sophieqgu@gmail.com>
2021-01-12 02:42:12 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -8,32 +8,25 @@ document.addEventListener('DOMEventLoaded', () => {
   const likes = document.getElementByClassName('like');
   for (const like in likes) {
     like.addEventListener('click', function()) {
-      const heart = event.target;
-      
+      let heart = event.target;
+      if (heart.innerText == EMPTY_HEART) {
+        mimicServerCall()
+        .then(response => {
+          heart.innerText = FULL_HEART;
+          heart.setAttribute('class', 'activated-heart');
+        })
+        .catch(error => {
+          const modal = document.getElementById('modal');
+          modal.removeAttribute('class', 'hidden');
+          modal.innerText = error.message;
+          setTimeout(() => modal.setAttribute('class', 'hidden'), 5000);
+        })
+      }
     }
   }
 })
 
 
-
-
-function mimicServerCall() {
-  fetch('')
-  .then(response => response.json())
-  .then(json => {
-    if () {
-      event.target.addClass(activated-heart);
-    } else {
-      event.target.removeClass(activated-heart);
-    }
-  })
-  .catch(error => {
-    const modal = document.getElementById('modal');
-    modal.removeClass('hidden');
-    modal.innerText = error.message;
-    setTimeout(() => modal.addClass('hidden'), 5000);
-  })
-}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
